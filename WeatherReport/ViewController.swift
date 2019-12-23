d237d1892c1a8a0e3dcb811d37e9bcf7bff78b42
Krishna Singana <krishna.singana@capgemini.com>
2019-12-23 11:05:30 +0530

    Added project files.

---
+++ WeatherReport/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherReport
+//
+//  Created by Krishna Singana on 23/12/19.
+//  Copyright © 2019 Krishna Singana. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
